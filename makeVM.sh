1d88df6b0f4c8c85bc892d0bd0c561ce34c3d320
Harsh Deshpande <hade7393@colorado.edu>
2020-02-09 15:57:20 -0700

    Tested flag input, commented out Azure VM commands for now

--- makeVM.sh
+++ makeVM.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while getopts "g:nipd" arg; do 
+while getopts "g:n:i:p:d" arg; do 
 	case $arg in
 		g) RG=${OPTARG};;
 		n) NAME=${OPTARG};;
@@ -10,15 +10,15 @@ while getopts "g:nipd" arg; do
 	esac
 done
 
+echo $RG
 echo $PORTS
 
+#az login
 
-az login
-
-if [ "$DELETE" = true ] && [ -n $RG ] ; then
-	az group delete --name $RG
-else
-	az group create --name $RG --location westus
-	az vm create -n $NAME -g $RG --image $IMAGE
-	az vm open-port --port $PORTS --resource-group $RG --name $NAME
-fi
+#if [ "$DELETE" = true ] && [ -n $RG ] ; then
+	#az group delete --name $RG
+#else
+	#az group create --name $RG --location westus
+	#az vm create -n $NAME -g $RG --image $IMAGE
+	#az vm open-port --port $PORTS --resource-group $RG --name $NAME
+#fi
